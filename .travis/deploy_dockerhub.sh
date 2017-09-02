08cc9543f3b7884a14eec2293f9bd33f0e16b381
saptadip <informsapta@gmail.com>
2017-09-02 23:54:07 +0200

    Added MY_DOCKER_REPO variable.

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+
+MY_DOCKER_REPO="saptadip/flask_order_placement_app"
+
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+docker build -f Dockerfile -t $MY_DOCKER_REPO:$TAG .
+docker push $MY_DOCKER_REPO
