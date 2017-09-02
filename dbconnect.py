93db0ef3cc81027e44f69d69b661540314b1a87a
Saptadip Sarkar <informsapta@gmail.com>
2017-09-02 22:04:32 +0200

    Modified summary section in dashboard.

--- dbconnect.py
+++ dbconnect.py
@@ -1,14 +1,10 @@
 import pymysql
 
-conn = pymysql.connect(host = "192.168.1.105",
-                           user = "sapta",
-                           passwd = "sapta123",
-                           db = "pythondb")
 
-cur = conn.cursor()
-cur.execute("SELECT * from customer")
-
-for r in cur:
-    print(r)
-cur.close()
-conn.close()
+def connection():
+    conn = pymysql.connect(host="192.168.1.105",
+                           user="sapta",
+                           passwd="sapta123",
+                           db="pythondb")
+    c = conn.cursor()
+    return c, conn
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from dbconnect import connection
+from pymysql import escape_string as thwart
+import gc
 
 app = Flask(__name__)
 
@@ -7,9 +10,33 @@ def homepage():
     return render_template("main.html")
 
 
-@app.route('/dashboard/')
+@app.route('/dashboard/', methods=["GET", "POST"])
 def dashboard():
-    return render_template("dashboard.html")
+    try:
+        c, conn = connection()
+        summary = dict()
+
+        c.execute("SELECT COUNT(*) FROM orderlist WHERE status='cancelled'")
+        cancelled_order = c.fetchone()[0]
+        summary['cancelled_order'] = cancelled_order
+
+        c.execute("SELECT COUNT(*) FROM orderlist WHERE status='pending'")
+        pending_order = c.fetchone()[0]
+        summary['pending_order'] = pending_order
+
+        c.execute("SELECT COUNT(*) FROM orderlist WHERE status='complete'")
+        complete_order = c.fetchone()[0]
+        summary['complete_order'] = complete_order
+
+
+        # close database connection
+        c.close()
+        conn.close()
+        gc.collect()
+        return render_template("dashboard.html", summary=summary)
+
+    except Exception as e:
+        return str(e)
 
 
 
