2dfeea7f2dea149be9dc6c66b7b9f4d2a6c6ba36
saptadip <informsapta@gmail.com>
2017-09-05 23:47:09 +0200

    Updated Application Deployment Process

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+echo "Installing Heroku toolbelt"
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
-docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
+#heroku plugins:install heroku-container-registry
+#docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+#heroku container:push web --app $HEROKU_APP_NAME
+
+echo "Setting up Heroku Git repo to: $HEROKU_APP_NAME"
+heroku git:remote -a $HEROKU_APP_NAME
+
+echo "Deploying application to Heroku..."
+git add .
+git commit -m "Deploying Flask App to Heroku"
+git push heroku master
